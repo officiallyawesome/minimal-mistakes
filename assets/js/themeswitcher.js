1cecee91d80aa1cbec6237e4db4e28afc7619703
Owen Clarke <o.clarke@gmail.com>
2020-05-22 11:33:55 +0100

    Fix to theme switcher

--- assets/js/themeswitcher.js
+++ assets/js/themeswitcher.js
@@ -5,52 +5,51 @@ var colorscheme = document.getElementsByName('colorscheme');
 
 /* changes the active radiobutton */
 function indicateTheme(mode) {
-    for(var i = colorscheme.length; i--; ) {
-        if(colorscheme[i].value == mode) {
-            colorscheme[i].checked = true;
-        }
+  for (var i = colorscheme.length; i--;) {
+    if (colorscheme[i].value == mode) {
+      colorscheme[i].checked = true;
     }
+  }
 }
 
 /* turns alt stylesheet on/off */
 function applyTheme(mode) {
-    var st = document.getElementById('theme-light');
-    if (mode == ALT_THEME) {
-        st.setAttribute('media', 'all');
-    }
-    else {
-        st.setAttribute('media', 'speech');
-    }
+  var active = document.getElementById(STORAGE_KEY + '-' + mode);
+  var otherMode = ALT_THEME;
+  if (mode == ALT_THEME) {
+    otherMode = DEFAULT_THEME;
+  }
+  var other = document.getElementById(STORAGE_KEY + '-' + otherMode);
+  active.setAttribute('media', 'all');
+  other.setAttribute('media', 'speech');
 }
 
 /* handles radiobutton clicks */
 function setTheme(e) {
-    var mode = e.target.value;
-    if (mode == 'auto') {
-        localStorage.removeItem(STORAGE_KEY);
-    }
-    else {
-        localStorage.setItem(STORAGE_KEY, mode);
-    }
-    /* when the auto button was clicked the auto-switcher needs to kick in */
-    var e = window.matchMedia('(prefers-color-scheme: ' + ALT_THEME + ')');
-    autoTheme(e);
+  var mode = e.target.value;
+  if (mode == 'auto') {
+    localStorage.removeItem(STORAGE_KEY);
+  } else {
+    localStorage.setItem(STORAGE_KEY, mode);
+  }
+  /* when the auto button was clicked the auto-switcher needs to kick in */
+  var e = window.matchMedia('(prefers-color-scheme: ' + ALT_THEME + ')');
+  autoTheme(e);
 }
 
 /* handles the media query evaluation, so it expects a media query as parameter */
 function autoTheme(e) {
-    var current = localStorage.getItem(STORAGE_KEY);
-    var mode = 'auto';
-    var indicate = 'auto';
-    /* user set preference has priority */
-    if ( current != null) {
-        indicate = mode = current;
-    }
-    else if (e != null && e.matches) {
-        mode = ALT_THEME;
-    }
-    applyTheme(mode);
-    indicateTheme(indicate);
+  var current = localStorage.getItem(STORAGE_KEY);
+  var mode = 'auto';
+  var indicate = 'auto';
+  /* user set preference has priority */
+  if (current != null) {
+    indicate = mode = current;
+  } else if (e != null && e.matches) {
+    mode = ALT_THEME;
+  }
+  applyTheme(mode);
+  indicateTheme(indicate);
 }
 
 /* create an event listener for media query matches and run it immediately */
@@ -59,12 +58,12 @@ autoTheme(mql);
 mql.addListener(autoTheme);
 
 /* set up listeners for radio button clicks */
-for(var i = colorscheme.length; i--; ) {
-    colorscheme[i].onclick = setTheme;
+for (var i = colorscheme.length; i--;) {
+  colorscheme[i].onclick = setTheme;
 }
 
 /* display theme switcher form(s) */
 var themeforms = document.getElementsByClassName("page__footer-theme");
-for(var i = themeforms.length; i--; ) {
-    themeforms[i].style.display = 'inline-block';
+for (var i = themeforms.length; i--;) {
+  themeforms[i].style.display = 'inline-block';
 }
